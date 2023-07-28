ed3934b23c005ec6f3a65f5c3c6ff444eac52491
Wendall Cada <wendallc@83864.com>
2023-07-28 13:19:42 -0700

    Remove redundant render call.

--- Fabric/src/main/java/betterdays/event/ClientEventListener.java
+++ Fabric/src/main/java/betterdays/event/ClientEventListener.java
@@ -15,9 +15,8 @@ public class ClientEventListener {
 
         ScreenEvents.AFTER_INIT.register((minecraft, screen, scaledWidth, scaledHeight) -> {
             if (screen instanceof InBedChatScreen) {
-                SleepGui.onGuiEvent(minecraft, screen);
                 ScreenEvents.afterRender(screen).register(((renderScreen, matrices, mouseX, mouseY, tickDelta) -> {
-                    SleepGui.renderSleepInterface(minecraft, renderScreen);
+                    SleepGui.onGuiEvent(minecraft, renderScreen);
                 }));
             }
         });
