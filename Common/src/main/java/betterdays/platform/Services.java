7dde56fbf0a2462805a6831f380ed0e1e6d3132a
Wendall Cada <wendallc@83864.com>
2025-09-13 10:12:31 -0700

    Extend services correctly.

--- Common/src/main/java/betterdays/platform/Services.java
+++ Common/src/main/java/betterdays/platform/Services.java
@@ -5,7 +5,7 @@ import betterdays.platform.services.IClientPlatform;
 import betterdays.platform.services.IPlatform;
 import betterdays.platform.services.IRegistryFactory;
 
-public class Services extends technology.roughness.whitenoise.platform.Services {
+public class Services extends technology.roughness.whitenoise.platform.ServicesBase {
 
     public static final IClientPlatform CLIENT_PLATFORM = load(BetterDays.LOGGER, IClientPlatform.class);
     public static final IPlatform PLATFORM = load(BetterDays.LOGGER, IPlatform.class);
